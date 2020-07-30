9915cae85ae240e0a9373216b4082b0333d730ef
JannIntveen <JannIntveen@gmail.com>
2020-07-30 15:37:57 +0200

    fix dependencies

--- src/main/java/thkoeln/st/springtestlib/testing/ObjectValidator.java
+++ src/main/java/thkoeln/st/springtestlib/testing/ObjectValidator.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+
 public class ObjectValidator {
 
     private ObjectBuilder objectBuilder;
