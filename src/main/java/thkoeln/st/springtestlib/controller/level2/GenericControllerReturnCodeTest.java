cf95072774d021f5bfc81aab46b2af97838dfd13
JannIntveen <JannIntveen@gmail.com>
2020-08-05 14:22:12 +0200

    reduce test method parameter in lvl 2 return code tests

--- src/main/java/thkoeln/st/springtestlib/controller/level2/GenericControllerReturnCodeTest.java
+++ src/main/java/thkoeln/st/springtestlib/controller/level2/GenericControllerReturnCodeTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.context.WebApplicationContext;
 import thkoeln.st.springtestlib.core.Attribute;
 import thkoeln.st.springtestlib.core.GenericTests;
+import thkoeln.st.springtestlib.core.ObjectDescription;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -32,50 +33,50 @@ public class GenericControllerReturnCodeTest extends GenericTests {
     }
 
     //Falsche URI, 404 erwartet
-    public void wrongUriTest(String WrongRestPath, String classPath, Attribute[] attributes) throws Exception {
+    public void wrongUriTest(ObjectDescription objectDescription) throws Exception {
         // Save Object
-        CrudRepository repository = oir.getRepository(classPath);
-        Object object = objectBuilder.buildObject(classPath, attributes);
+        CrudRepository repository = oir.getRepository(objectDescription.getClassPath());
+        Object object = objectBuilder.buildObject(objectDescription);
         repository.save(object);
 
         // Perform get
         ResultActions resultActions = mockMvc
-                .perform(get(WrongRestPath + "/" + oir.getId(object)).contentType(MediaType.APPLICATION_JSON))
+                .perform(get("/invalid/URI/" + oir.getId(object)).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
     }
 
     //Falsche ID, 404 erwartet
-    public void wrongIDTest(String restPath, String classPath, Attribute[] attributes) throws Exception {
+    public void wrongIDTest(ObjectDescription objectDescription) throws Exception {
         // Perform get
         ResultActions resultActions = mockMvc
-                .perform(get(restPath + "/" + UUID.randomUUID()).contentType(MediaType.APPLICATION_JSON))
+                .perform(get(objectDescription.getRestPath() + "/" + UUID.randomUUID()).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
     }
 
     //Delete nicht erlaubt, 405 erwartet
-    public void deleteMethodNotAllowedTest(String restPath, String classPath, Attribute[] attributes) throws Exception {
+    public void deleteMethodNotAllowedTest(ObjectDescription objectDescription) throws Exception {
         // Save Object
-        CrudRepository repository = oir.getRepository(classPath);
-        Object object = objectBuilder.buildObject(classPath, attributes);
+        CrudRepository repository = oir.getRepository(objectDescription.getClassPath());
+        Object object = objectBuilder.buildObject(objectDescription);
         repository.save(object);
 
         // Perform delete
         mockMvc
-                .perform(delete(restPath + "/" + oir.getId(object)))
+                .perform(delete(objectDescription.getRestPath() + "/" + oir.getId(object)))
                 .andExpect(status().isMethodNotAllowed());
 
         Optional<Object> objectOp = repository.findById(oir.getId(object));
         assertTrue(objectOp.isPresent());
     }
 
-    public void invalidValueTest(String restPath, String classPath, Attribute[] attributes, Attribute[] hiddenAttributes) throws Exception {
+    public void invalidValueTest(ObjectDescription objectDescription) throws Exception {
         // Create Object
-        Object object = objectBuilder.buildObject(classPath, attributes);
+        Object object = objectBuilder.buildObject(objectDescription);
 
         // Perform Post
         ResultActions resultActions = mockMvc
                 .perform(
-                        post(restPath)
+                        post(objectDescription.getRestPath())
                                 .content(objectMapper.writeValueAsString(object))
                                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isUnprocessableEntity());
